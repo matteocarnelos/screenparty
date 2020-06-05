c31624ab4d69d82dea9896c3d4f7e19160b321d5
Matteo Carnelos <matteo.carnelos98@gmail.com>
2020-06-05 19:58:34 +0200

    Changes:
    + added app icon
    + added Material Design style

--- app/src/main/java/it/unipd/dei/es/screenparty/ui/ClientFragment.java
+++ app/src/main/java/it/unipd/dei/es/screenparty/ui/ClientFragment.java
@@ -1,6 +1,5 @@
 package it.unipd.dei.es.screenparty.ui;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
@@ -20,6 +19,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
 
 import it.unipd.dei.es.screenparty.R;
@@ -29,6 +29,7 @@ import it.unipd.dei.es.screenparty.party.PartyManager;
 public class ClientFragment extends Fragment {
 
     private TextInputLayout hostIpField;
+    private Dialogs dialogs = new Dialogs();
 
     private NavController navController;
     private PartyManager partyManager = PartyManager.getInstance();
@@ -43,66 +44,82 @@ public class ClientFragment extends Fragment {
 
     private Handler handler = new Handler(Looper.getMainLooper()) {
         @Override
-        public void handleMessage(Message inputMessage) {
-            switch (inputMessage.what) {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
                 case NetworkEvents.JOIN_FAILED:
-                    new AlertDialog.Builder(getActivity())
-                            .setMessage((String)inputMessage.obj)
-                            .setTitle("Join failed")
-                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    partyManager.stop();
-                                }
-                            })
-                            .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    partyManager.restart();
-                                }
-                            })
-                            .show();
+                    dialogs.showJoinFailedDialog((String)msg.obj);
                     break;
                 case NetworkEvents.Client.PARTY_JOINED:
                     navController.navigate(R.id.actionToPrepare);
                     break;
                 case NetworkEvents.Client.PARTY_FULL:
-                    new AlertDialog.Builder(getActivity())
-                            .setMessage("The party you are trying to connect is full")
-                            .setTitle("Party full")
-                            .setPositiveButton("OK", null)
-                            .show();
+                    dialogs.showPartyFullDialog();
                     break;
                 case NetworkEvents.Client.HOST_LEFT:
-                    new AlertDialog.Builder(getActivity())
-                            .setMessage("The host has left the party")
-                            .setTitle("You are alone")
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    partyManager.stop();
-                                    navController.popBackStack(R.id.startFragment, false);
-                                }
-                            })
-                            .show();
+                    dialogs.showHostLeftDialog();
                     break;
                 case NetworkEvents.COMMUNICATION_FAILED:
-                    new AlertDialog.Builder(getActivity())
-                            .setMessage((String)inputMessage.obj)
-                            .setTitle("Communication failed")
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    partyManager.stop();
-                                    navController.popBackStack(R.id.startFragment, false);
-                                }
-                            })
-                            .show();
+                    dialogs.showCommunicationFailedDialog((String)msg.obj);
                     break;
-                default: super.handleMessage(inputMessage);
+                default: super.handleMessage(msg);
             }
         }
     };
+
+    private class Dialogs {
+
+        private void showJoinFailedDialog(String message) {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setMessage(message)
+                    .setTitle("Join failed")
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            partyManager.stop();
+                        }
+                    })
+                    .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            partyManager.restart();
+                        }
+                    }).show();
+        }
+
+        private void showPartyFullDialog() {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setMessage("The party you are trying to connect is full")
+                    .setTitle("Party full")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+
+        private void showHostLeftDialog() {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setMessage("The host has left the party")
+                    .setTitle("You are alone")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            partyManager.stop();
+                            navController.popBackStack(R.id.startFragment, false);
+                        }
+                    }).show();
+        }
+
+        private void showCommunicationFailedDialog(String message) {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setMessage(message)
+                    .setTitle("Communication failed")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            partyManager.stop();
+                            navController.popBackStack(R.id.startFragment, false);
+                        }
+                    }).show();
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/it/unipd/dei/es/screenparty/ui/HostFragment.java
+++ app/src/main/java/it/unipd/dei/es/screenparty/ui/HostFragment.java
@@ -1,11 +1,16 @@
 package it.unipd.dei.es.screenparty.ui;
 
-import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,13 +24,24 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.io.IOException;
+
 import it.unipd.dei.es.screenparty.R;
+import it.unipd.dei.es.screenparty.media.MediaParams;
 import it.unipd.dei.es.screenparty.network.NetworkEvents;
 import it.unipd.dei.es.screenparty.party.PartyManager;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
 public class HostFragment extends Fragment {
 
+    private static final int SELECT_MEDIA_REQUEST_CODE = 0;
+
     private TextView hostIpLabel;
+    private Dialogs dialogs = new Dialogs();
 
     private NavController navController;
     private PartyManager partyManager = PartyManager.getInstance();
@@ -43,39 +59,16 @@ public class HostFragment extends Fragment {
         public void handleMessage(@NonNull Message msg) {
             switch(msg.what) {
                 case NetworkEvents.Host.NOT_STARTED:
-                    new AlertDialog.Builder(getActivity())
-                            .setTitle("Could not start the server")
-                            .setMessage((String)msg.obj)
-                            .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    partyManager.restart();
-                                }
-                            })
-                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    partyManager.stop();
-                                    navController.popBackStack(R.id.startFragment, false);
-                                }
-                            }).show();
+                    dialogs.showNotStartedDialog((String)msg.obj);
                     break;
                 case NetworkEvents.Host.WAITING_DEVICES:
                     hostIpLabel.setText((String)msg.obj);
                     break;
                 case NetworkEvents.CONNECTION_FAILED:
-                    new AlertDialog.Builder(getActivity())
-                            .setTitle("Connection failed")
-                            .setMessage((String)msg.obj)
-                            .setPositiveButton("OK", null)
-                            .show();
+                    dialogs.showConnectionFailedDialog((String)msg.obj);
                     break;
                 case NetworkEvents.JOIN_FAILED:
-                    new AlertDialog.Builder(getActivity())
-                            .setTitle("Join failed")
-                            .setMessage((String)msg.obj)
-                            .setPositiveButton("OK", null)
-                            .show();
+                    dialogs.showJoinFailedDialog((String)msg.obj);
                     break;
                 case NetworkEvents.Host.CLIENT_JOINED:
                     Toast.makeText(getContext(), "A device has joined the party!", Toast.LENGTH_LONG).show();
@@ -87,22 +80,86 @@ public class HostFragment extends Fragment {
                     Toast.makeText(getContext(), "A device has left the party", Toast.LENGTH_LONG).show();
                     break;
                 case NetworkEvents.COMMUNICATION_FAILED:
-                    new AlertDialog.Builder(getActivity())
-                            .setTitle("Communication failed")
-                            .setMessage((String)msg.obj)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    partyManager.stop();
-                                    navController.popBackStack(R.id.startFragment, false);
-                                }
-                            })
-                            .show();
+                    dialogs.showCommunicationFailedDialog((String)msg.obj);
                     break;
                 default: super.handleMessage(msg);
             }
         }
     };
+
+    private class Dialogs {
+
+        private void showNotStartedDialog(String message) {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Could not start the server")
+                    .setMessage(message)
+                    .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            partyManager.restart();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            backPressedCallback.handleOnBackPressed();
+                        }
+                    }).show();
+        }
+
+        private void showConnectionFailedDialog(String message) {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Connection failed")
+                    .setMessage(message)
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+
+        private void showJoinFailedDialog(String message) {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Join failed")
+                    .setMessage(message)
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+
+        private void showCommunicationFailedDialog(String message) {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Communication failed")
+                    .setMessage(message)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            backPressedCallback.handleOnBackPressed();
+                        }
+                    })
+                    .show();
+        }
+
+        private void showMediaErrorDialog(String message) {
+            new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle("Media error")
+                    .setMessage(message)
+                    .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            backPressedCallback.handleOnBackPressed();
+                        }
+                    })
+                    .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            openMediaPicker();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            backPressedCallback.handleOnBackPressed();
+                        }
+                    }).show();
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -115,16 +172,68 @@ public class HostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_host, container, false);
-
         hostIpLabel = view.findViewById(R.id.host_ip_label);
-
-        partyManager.startAsHost();
-
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         navController = Navigation.findNavController(view);
+        openMediaPicker();
+    }
+
+    private void openMediaPicker() {
+        Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        fileIntent.setType("image/* video/*");
+        fileIntent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"image/*", "video/*"});
+
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/* video/*");
+
+        Intent chooserIntent = Intent.createChooser(fileIntent, "Select the media you want to play");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] { galleryIntent });
+
+        startActivityForResult(chooserIntent, SELECT_MEDIA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == SELECT_MEDIA_REQUEST_CODE) {
+            if(resultCode == RESULT_CANCELED) backPressedCallback.handleOnBackPressed();
+            else if(resultCode == RESULT_OK) {
+                Uri selectedUri = data.getData();
+                if(selectedUri == null) return;
+
+                ContentResolver contentResolver = requireContext().getContentResolver();
+                String type = contentResolver.getType(selectedUri);
+                if(type == null || !type.startsWith("image") && !type.startsWith("video")) {
+                    dialogs.showMediaErrorDialog("Invalid media type, please try again with another one");
+                    return;
+                }
+                MediaParams.Type mediaType = type.startsWith("image") ? MediaParams.Type.IMAGE : MediaParams.Type.VIDEO;
+
+                Bitmap bitmap;
+                if(mediaType == MediaParams.Type.VIDEO) {
+                    MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+                    retriever.setDataSource(getContext(), selectedUri);
+                    bitmap = retriever.getFrameAtTime();
+                    retriever.release();
+                } else {
+                    try { bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedUri); }
+                    catch (IOException ignored) {
+                        dialogs.showMediaErrorDialog("An error occurred during file processing, please try again");
+                        return;
+                    }
+                }
+                float aspectRatio = (float)bitmap.getWidth() / (float)bitmap.getHeight();
+
+                MediaParams mediaParams = new MediaParams(selectedUri, mediaType, aspectRatio);
+
+                PartyManager partyManager = PartyManager.getInstance();
+                partyManager.getPartyParams().setMediaParams(mediaParams);
+
+                partyManager.startAsHost();
+            }
+        }
     }
 }
