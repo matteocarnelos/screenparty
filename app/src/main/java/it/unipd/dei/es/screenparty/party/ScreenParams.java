fec9752d39448b14908f81c4489367a54bb81fe8
Alessandro Fano <alessandrofano98@gmail.com>
2020-06-11 18:05:04 +0200

    Changes: + added ... - removed ... ~ modified ...

--- app/src/main/java/it/unipd/dei/es/screenparty/party/ScreenParams.java
+++ app/src/main/java/it/unipd/dei/es/screenparty/party/ScreenParams.java
@@ -13,8 +13,8 @@ public class ScreenParams {
     /**
      * Constructor of {@link ScreenParams}. Create a new {@link ScreenParams} and sets the width and
      * the height as indicated by the given parameters.
-     * @param width: The width of the screen.
-     * @param height: The height of the screen.
+     * @param width: The width of the screen in inches.
+     * @param height: The height of the screen in inches.
      */
     public ScreenParams(float width, float height) {
         this(width, height, 0, 0);
@@ -24,10 +24,10 @@ public class ScreenParams {
      * Constructor of {@link ScreenParams}. Create a new {@link ScreenParams} and sets the width,
      * the height, the horizontal pixel density and the vertical pixel density as indicated by the
      * given parameters.
-     * @param width: The width of the screen.
-     * @param height: The height of the screen.
-     * @param xdpi: The horizontal pixel density of the screen.
-     * @param ydpi: The vertical pixel density of the screen.
+     * @param width: The width of the screen in inches.
+     * @param height: The height of the screen in inches.
+     * @param xdpi: The horizontal pixel density of the screen in dot per inches.
+     * @param ydpi: The vertical pixel density of the screen in dot inches.
      */
     public ScreenParams(float width, float height, float xdpi, float ydpi) {
         this.width = width;
@@ -38,14 +38,14 @@ public class ScreenParams {
 
     /**
      * Get the width of the screen.
-     * @return The width of the screen.
+     * @return The width of the screen in inches.
      */
     public float getWidth() {
         return width;
     }
 
     /**
-     * Get the height of the screen.
+     * Get the height of the screen in inches.
      * @return The height of the screen.
      */
     public float getHeight() {
@@ -54,7 +54,7 @@ public class ScreenParams {
 
     /**
      * Get the horizontal pixel density of the screen.
-     * @return The horizontal pixel density of the screen.
+     * @return The horizontal pixel density of the screen in dot inches.
      */
     public float getXdpi() {
         return xdpi;
@@ -62,7 +62,7 @@ public class ScreenParams {
 
     /**
      * Get the vertical pixel density of the screen.
-     * @return The vertical pixel density of the screen.
+     * @return The vertical pixel density of the screen in dot per inches.
      */
     public float getYdpi() {
         return ydpi;
@@ -70,7 +70,7 @@ public class ScreenParams {
 
     /**
      * Sets the height of the screen.
-     * @param height: The new height of the screen.
+     * @param height: The new height of the screen in inches.
      */
     public void setHeight(float height) {
         this.height = height;
